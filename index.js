c132fae84333eeb04341b3082ad931ba50220a15
lairias <54221775+lairias@users.noreply.github.com>
2022-04-08 11:44:33 -0600

    tes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
+require('dotenv').config()
 const ruta = require("./router.js")
 app.use("/api",ruta)
 
-app.listen(3000,()=>{console.log("Servidor iniciado")})
+app.listen(process.env.PORT ,()=>{console.log("Servidor iniciado")})
